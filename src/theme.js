02b54feee4e135bf3d20685f29f6867994393ba4
martinness <martin.ness@icloud.com>
2023-10-05 23:17:09 +0200

    progressed

--- src/theme.js
+++ src/theme.js
@@ -108,11 +108,11 @@ export const useMode = () => {
   const colorMode = useMemo(
     () => ({
       toggleColorMode: () => 
-        setMode((prev)) => (prev === "light" ? "dark" : "light")),
+        setMode((prev) => (prev === "light" ? "dark" : "light")),
     }),
     []
   );
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return [theme, colorMode];
-}
+};
